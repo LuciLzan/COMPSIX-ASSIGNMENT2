44657cfe3af2d0ad74f39bc2d5d7770fc61fad17
Luke Laurenzana <laurenzana.luke@ic.edu>
2025-11-02 01:27:53 -0500

    Number processor and text anylyzer functional

---
+++ src/numberProcessor.js
@@ -0,0 +1,64 @@
+/**
+ * Step 7
+ * In src/numberProcessor.js, create a script that reads a file containing numbers (one per line) and processes them:
+ *
+ * Calculate the sum of all numbers
+ * Find the highest and lowest numbers
+ * Calculate the average
+ * 💡 So that you can easily create unit tests, you should create different functions for each of these and return the result from the function.
+ */
+const fs = require('fs');
+
+let sampleNumbers = "../data/sample-numbers.txt"
+
+function calculateSum(file) {
+    let str = fs.readFileSync(file, 'utf8')
+    let sum = 0
+    /*
+    The matcher doesn't really need to include . or , but if we're using
+    parseFloat() we might as well
+     */
+    str.match(/([0-9]|.|,)+/g).forEach((item) => {
+        //They are all ints, would parseInt(item) not make more sense?
+        //It's alright I'll just follow the guide :)
+        sum += parseFloat(item);
+    })
+    return sum;
+}
+function findHighest(file) {
+    let str = fs.readFileSync(file, 'utf8')
+    return str
+        .match(/([0-9]|.|,)+/g)
+        .map((strNum)=>parseFloat(strNum))
+        .sort((a, b) => a - b)
+        [0];
+}
+function findLowest(file) {
+    let str = fs.readFileSync(file, 'utf8')
+    return str
+        .match(/([0-9]|.|,)+/g)
+        .map((strNum) => parseFloat(strNum))
+        .sort((a, b) => b - a)
+        [0];
+}
+function findAverage(file) {
+    let str = fs.readFileSync(file, 'utf8')
+    let total = str.match(/([0-9]|.|,)+/g).length
+    /*
+    As much as I'd love to just calculateSum(file) here, I
+    don't think it would be worth an entire other read of the file
+    to type a few less lines
+    */
+    let sum = 0
+    str.match(/([0-9]|.|,)+/g).forEach((item) => {
+        sum += parseFloat(item)
+    })
+    return sum/total;
+}
+
+//Running the functions
+
+console.log(`Sum of numbers in "${sampleNumbers}": ${calculateSum(sampleNumbers)}`);
+console.log(`Largest number in "${sampleNumbers}": ${findHighest(sampleNumbers)}`);
+console.log(`Smallest number in "${sampleNumbers}": ${findLowest(sampleNumbers)}`);
+console.log(`Average of "${sampleNumbers}": ${findAverage(sampleNumbers)}`);
---
+++ src/textAnalyzer.js
@@ -0,0 +1,39 @@
+/**
+ * In src/textAnalyzer.js script that reads a text file and analyzes it. Create functions that do the following:
+ *
+ * Count the total number of words
+ * Find the longest word
+ * Count how many lines the file has
+ * 💡 So that you can easily create unit tests, you should create different functions for each of these and return the result from the function.
+ */
+
+const quotes = "../data/quotes.txt"
+const sampleText = "../data/sample-text.txt"
+
+const fs = require('fs');
+
+function countWords(file) {
+    let str = fs.readFileSync(file, 'utf8').trim();
+    //Empty file = no words
+    //Count the spaces that have a not space after
+    return str.length > 0 ? str.match(/\w+/g).length+1 : 0
+
+}
+function findLongestWord(file) {
+    return fs.readFileSync(file, 'utf8')
+        .match(/\w+/g)
+        .sort((a, b) => b.length - a.length)[0];
+}
+
+function countLines(file) {
+    let arr = fs.readFileSync(file, 'utf8').match(/\n/g)
+    return arr ? arr.length+1: 1
+}
+
+//Use the funcitons
+console.log(`Word count of ${sampleText}:  ${countWords(sampleText)}`);
+console.log(`Longest word in ${sampleText}:  ${findLongestWord(sampleText)}`);
+console.log(`Lines in ${sampleText}:  ${countLines(sampleText)}`);
+console.log(`Word count of ${quotes}:  ${countWords(quotes)}`);
+console.log(`Longest word in ${quotes}:  ${findLongestWord(quotes)}`);
+console.log(`Lines in ${quotes}:  ${countLines(quotes)}`);
